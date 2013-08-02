076191d261b4d19eb868bd942cad1712c39fa6c0
Robert Kehoe <robert.kehoe@f-secure.com>
2013-08-02 17:56:03 +0800

    Implementing ajax loading and menu headers

--- js/menu-adv.js
+++ js/menu-adv.js
@@ -1,59 +1,63 @@
+/**
+ * User: Robert Kehoe
+ * Date: 02/Aug/13 16:37
+ */
 
-var menu = [
-  {
-    title : "Fs Radar",
-    url   : "../fs-radar/"
-  },
-  {
-    title : "Lokki",
-    url   : "../lokki/"
-  },
-  {
-    title : "F-Secure Key",
-    url   : "../fs-key/"
-  },
-  {
-    title : "Content Anywhere",
-    url   : "../fip-can/"
-  },
-  {
-    title : "Mobile Security",
-    url   : "../mobile-security/"
-  },
-  {
-    title : "Internet Security",
-    url   : "../safe-anywhere/"
-  }
+var namespace = {};
 
-];
+$(document).ready(function () {
+
+  // When page loads send Ajax query to get menu
+  $.ajax({
+    url     : "js/menu.json",
+    success : function (data) {
+      namespace.menu = data;
+      namespace.buildMenu(); // dynamically insert the menu
+    }
+  });
+});
+
+$(document).on("pageshow", function () {
+  namespace.buildMenu();
+});
 
 
-// Has to be on "pageshow" rather than init, in order to update the current menu highlighting properly
-$(document).on("pageshow", function (event) {
+namespace.buildMenu = function () {
+
+  // Select and cache all "empty" menus. Each page contains an empty menu
+  // jqm Ajax preloading of pages will cause multiple pages to be present in the DOM (but only current visible)
+  var ul = $(".mainMenu:empty");
 
 
-  var ul = $(".mainMenu:empty");  // cache the "new" ul menu element
+  // Handle initial case, where Ajax query not yet sent
+  if (namespace.menu != undefined) {
 
-  if (ul.length > 0) { // only operate on pages where menu not already addded
-
+    // Add a readonly header item
     var items = '<li data-role="list-divider" role="heading" class="ui-li ui-li-divider ui-bar-d ui-first-child">Navigation</li>';
     var parts = [];
 
-    for (var i = 0; i < menu.length; i++) {
+    for (var i = 0; i < namespace.menu.length; i++) {
 
-      parts = window.location.pathname.split('/');
-      if (parts[parts.length-2] == menu[i].url.split("/")[menu[i].url.split("/").length-2]) {
-        // Highlight current page
-        items += '<li data-theme="e"><a href="' + menu[i].url + '">&nbsp; ' + menu[i].title + '</a></li>'
+      // Handle header/section pages differently
+      if (namespace.menu[i].header) {
+        items += '<li data-role="list-divider" role="heading" class="ui-li ui-li-divider ui-bar-d ui-first-child">' + namespace.menu[i].title + '</li>';
       } else {
-        items += '<li><a href="' + menu[i].url + '">' + menu[i].title + '</a></li>'
+
+        // Get current location (to be able to highlight the correct item)
+        parts = window.location.pathname.split('/');
+
+        // You will almost certainly have to rework this if() statement to suit your folder / site structure
+        if (namespace.menu[i].url == parts[parts.length - 1]) {
+          // Highlight current page
+          items += '<li data-theme="e"><a href="' + namespace.menu[i].url + '">&nbsp; ' + namespace.menu[i].title + '</a></li>'
+        } else {
+          items += '<li><a href="' + namespace.menu[i].url + '">' + namespace.menu[i].title + '</a></li>'
+        }
       }
     }
 
     ul.append(items);
-    ul.listview('refresh'); // use cache, as .mainMenu:empty will no longer work (append called!)
-
+    ul.listview('refresh'); // use cache, as ".mainMenu:empty" will no longer work (append called!)
   }
 
-
-});
+};
